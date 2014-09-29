806644b6d9c7f8458be5a204b98388325c4c0a20
Benjamin Pearce <hawk.git@bcpearce.com>
2014-09-28 22:23:38 -0400

    added tests for validation

--- spec/code_spec.rb
+++ spec/code_spec.rb
@@ -2,18 +2,29 @@ require_relative "../lib/code.rb"
 
 describe Code do 
 
-  describe "Randomized code" do
-
-    before do
-      @code = Code.new.randomize
-    end
-
-    it { should respond_to "randomize" }
-
-    it "should contain only default colors" do
-      expect{ @code.sequence.all? { |code_item| code_item == Code.default_colors.any? } }.to be_true
-    end
-  
+  before do
+    @code = Code.new(4)
   end
+
+  it "should contain only default colors" do
+    expect{ @code.sequence.all? { |code_item| code_item == Code.default_colors.any? } }.to be_true
+  end
+
+  describe "when a sequence is valid" do
+    subject{ @code.valid? }
+
+    it { should be_true }
+  end
+
+  describe "when a sequence is invalid" do
+    before do
+      @code.sequence[1] = "apple"
+    end
+
+    subject { @code.valid? }
+
+    it { should_not be_true }
+  end
+
 
 end
