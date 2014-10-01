02d74d9c8a0e25876ec593cd1820ec83bd7cf620
Benjamin Pearce <hawk.git@bcpearce.com>
2014-09-30 22:06:41 -0400

    added AI class

---
+++ lib/ai.rb
@@ -0,0 +1,11 @@
+class AI < Player
+
+  CODE_LEN = 4
+
+  def initialize 
+    # map out all of the possible guesses for 
+    @guessables = Code.default_colors.repeated_combination(CODE_LEN).to_a
+    super
+  end
+
+end
