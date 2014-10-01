97253f08643b1f437d7f971ffd622a25a8cd322e
Benjamin Pearce <hawk.git@bcpearce.com>
2014-09-30 22:30:01 -0400

    added initial guess case

--- lib/ai.rb
+++ lib/ai.rb
@@ -10,4 +10,20 @@ class AI < Player
     super
   end
 
+  def guess
+
+    guess = Array.new(CODE_LEN)
+
+    # exception case for when it is the first guess
+    # it will start by guessing a random code of type 'AABB'
+    if @guess_hist.empty?
+      guess[0], guess[2] = Code.default_colors[rand(0..5)].to_s, Code.default_colors[rand(0..5)].to_s
+      guess[1], guess[3] = guess[0], guess[2]
+      @code_guess = Code.new(guess.join(" "))
+    end
+
+    @guess_hist << @code_guess
+
+  end
+
 end
--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -1,11 +1,21 @@
 require_relative "../lib/ai.rb"
 
 describe AI do    
+
+  before { @ai = AI.new }
   
   it "should contain a selection map" do
-    @ai = AI.new
     expect(@ai.instance_variable_get(:@guessables)).to be_true
   end
 
+  describe "making a guess" do
+
+    it "should being guessing with an 'AABB' Code" do
+      @ai.guess
+      first_guess = @ai.guess_hist[0].sequence
+      expect([first_guess[0], first_guess[2]]).to eq([first_guess[1], first_guess[3]])
+    end
+
+  end
 
 end
